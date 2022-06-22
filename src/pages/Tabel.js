2055cc744bbaf5fa2e02c252aa198942d0ce1989
Nynke <35605149+Nynkek@users.noreply.github.com>
2022-06-22 13:50:07 +0200

    tabel filteren werkt goed

--- src/pages/Tabel.js
+++ src/pages/Tabel.js
@@ -2,30 +2,22 @@ import React, {useEffect, useState} from 'react';
 import DataTable from 'react-data-table-component';
 import axios from "axios";
 import "../components/tabel/tabel.css";
-import {Link} from "react-router-dom";
 import FilterComponent from "../components/tabel/FilterComponent";
 
-function Tabel(props) {
-    const [menstrualDiscs, setMenstrualDiscs] = useState({
-        data: [],
-        unfilteredData: [],
-    });
+function Tabel() {
+    const [menstrualDiscs, setMenstrualDiscs] = useState([]);
     const [filteredDiscs, setFilteredDiscs] = useState([]);
     const [filterText, setFilterText] = React.useState('');
-    const [bmm, setBmm] = useState("");
-    const [herbuikbaar, setHerbuikbaar] = useState("");
-    const [steel, setSteel] = useState("");
-
+    const [bmm, setBmm] = useState('');
+    const [reusable, setReusable] = useState('');
+    const [stem, setStem] = useState('');
+    const [selectedOption, setSelectedOption] = useState()
 
     useEffect(() => {
         axios.get("http://localhost:8080/discs/")
             .then((response) => {
-                setMenstrualDiscs(
-                    {
-                        data: response.data,
-                        unfilteredData: response.data,
-                    }
-                );
+                setMenstrualDiscs(response.data);
+                setFilteredDiscs(response.data)
                 console.log(menstrualDiscs.data);
             }).catch(error => {
             console.error('There was an error!', error);
@@ -33,19 +25,6 @@ function Tabel(props) {
 
     }, []);
 
-
-    const subHeaderComponentMemo = React.useMemo(() => {
-        const handleClear = () => {
-            if (filterText) {
-                setFilterText('');
-            }
-        };
-
-        return (
-            <FilterComponent onFilter={e => setFilterText(e.target.value)} onClear={handleClear}
-                             filterText={filterText}/>
-        );
-    }, [filterText]);
 
     const columns = [
         {
@@ -63,7 +42,7 @@ function Tabel(props) {
             sortable: true,
             compact: true,
             wrap: true,
-            // width: "14ch",
+            width: "110px",
             cell: (row) => <a href={row.linkToStore} target="_blank"
                               rel="noopener noreferrer">{row.name} {row.model}</a>,
         },
@@ -73,7 +52,7 @@ function Tabel(props) {
             selector: row => row.width + "mm",
             sortable: true,
             compact: true,
-            // width: "9ch",
+            width: "60px",
             center: true,
         },
         {
@@ -81,7 +60,7 @@ function Tabel(props) {
             selector: row => row.capacity + "ml",
             sortable: true,
             compact: true,
-            // width: "8ch",
+            width: "60px",
             center: true,
         },
 
@@ -90,24 +69,24 @@ function Tabel(props) {
             selector: row => row.firmness.toLowerCase(),
             sortable: true,
             compact: true,
-            center: true,
-            // width: "10ch",
+            width: "70px",
+            hide: "md"
         },
         {
             name: 'Materiaal',
             selector: row => row.material.toLowerCase(),
             sortable: true,
             compact: true,
-            center: true,
-            // width: "10ch",
+            width: "70px",
+            hide: "md"
         },
         {
             name: 'Vorm',
             selector: row => row.shape.toLowerCase(),
             sortable: true,
             compact: true,
-            center: true,
-            // width: "8ch",
+            width: "60px",
+            hide: "md"
 
         },
         {
@@ -115,8 +94,7 @@ function Tabel(props) {
             selector: row => row.brand,
             sortable: true,
             compact: true,
-            center: true,
-            // width: "12ch",
+            width: "12ch",
             hide: "md"
 
         },
@@ -130,101 +108,115 @@ function Tabel(props) {
         },
 
     ];
-    const data = menstrualDiscs.data;
+    const data = filteredDiscs;
 
+    useEffect(() => {
+        let discList = menstrualDiscs;
 
-    function handleOptionChange(e) {
-        setBmm(e.target.value);
-        switch (e.target.value) {
+        switch (bmm) {
             case "hoge-bmm":
-                console.log(menstrualDiscs.data.width);
-                const discList1 = menstrualDiscs.unfilteredData;
-                setMenstrualDiscs(
-                    {
-                        data: discList1.filter(disc => disc.width > 70),
-                        unfilteredData: menstrualDiscs.unfilteredData,
-                    }
-                );
+                discList = discList.filter(disc => disc.width >= 68);
                 break;
             case "gem-bmm":
-                const discList2 = menstrualDiscs.unfilteredData;
-                setMenstrualDiscs(
-                    {
-                        data: discList2.filter(disc => disc.width > 65 && disc.width < 75),
-                        unfilteredData: menstrualDiscs.unfilteredData,
-                    }
-                );
+                discList = discList.filter(disc => disc.width > 65 && disc.width < 75);
                 break;
             case "lage-bmm":
-                const discList3 = menstrualDiscs.unfilteredData;
-                setMenstrualDiscs(
-                    {
-                        data: discList3.filter(disc => disc.width < 69),
-                        unfilteredData: menstrualDiscs.unfilteredData,
-                    }
-                );
+                discList = discList.filter(disc => disc.width < 68);
                 break;
             case "geen-bmm":
+                discList = discList.filter(disc => disc.width > 53);
+                break;
             default:
-                const discList4 = menstrualDiscs.unfilteredData;
-                setMenstrualDiscs(
-                    {
-                        data: discList4,
-                        unfilteredData: menstrualDiscs.unfilteredData,
-                    }
-                );
         }
+        switch (reusable) {
+            case "ja-hbb":
+                discList = discList.filter(disc => disc.reusable === true)
+                break;
+            case "nee-hbb":
+                discList = discList.filter(disc => disc.reusable === false)
+                break;
+            case "geen-hbb":
+            default:
+        }
+        switch (stem) {
+            case "ja-steel":
+                discList = discList.filter(disc => disc.hasStem === true)
+                break;
+            case "nee-steel":
+                break;
+            default:
+        }
+        setFilteredDiscs(discList);
 
+    }, [bmm, reusable, stem])
 
+    function handleOptionChange(e) {
+        if (e.target.name === "bmm") {
+            setBmm(e.target.value);
+        } else if (e.target.name === "herbruikbaar") {
+            setReusable(e.target.value);
+        } else if (e.target.name === "steel") {
+            setStem(e.target.value);
+        }
+        setSelectedOption(e.target.value);
     }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log("submit")
+    function resetFilters() {
+        setFilteredDiscs(menstrualDiscs);
+        setBmm('');
+        setReusable('');
+        setStem('');
     }
 
     return (
         <>
-            <h4>Baarmoedermondhoogte (bepaald de breedte van de disk)</h4>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="hoge-bmm">
+            <h4>Baarmoedermondhoogte (bepaalt de breedte van de disk)</h4>
+            <form>
+                <label>
                     <input type="radio" value="hoge-bmm" name="bmm"
-                           onChange={handleOptionChange}/>
+                           onChange={handleOptionChange} checked={bmm === "hoge-bmm"}/>
                     Ik heb een hoge bmm.
                 </label><br/>
-                <label htmlFor="gem-bmm">
-                    <input type="radio" value="gem-bmm" name="bmm" onChange={handleOptionChange}/>
+                <label>
+                    <input type="radio" value="gem-bmm" name="bmm" onChange={handleOptionChange}
+                           checked={bmm === "gem-bmm"}/>
                     Ik heb een gemiddelde bmm.
                 </label><br/>
-                <label htmlFor="lage-bmm">
+                <label>
                     <input type="radio" value="lage-bmm" name="bmm"
-                           onChange={handleOptionChange}/>
+                           onChange={handleOptionChange} checked={bmm === "lage-bmm"}/>
                     Ik heb een lage bmm.
                 </label><br/>
-                <label htmlFor="geen-bmm">
+                <label>
                     <input type="radio" value="geen-bmm" name="bmm"
-                           onChange={handleOptionChange}/>
+                           onChange={handleOptionChange} checked={bmm === "geen-bmm"}/>
                     Ik weet het niet, geef me gemiddelde disks.
                 </label><br/>
-                {console.log(bmm)}
+
                 <h4>Herbruikbaar?</h4>
-                <input type="radio" value="ja-hbb" name="herbruikbaar"/>
-                <label htmlFor="ja-hbb">Ik wil een herbruikbare disk (gaat tot 10 jaar mee)</label><br/>
-                <input type="radio" value="nee-hbb" name="herbruikbaar"/>
-                <label htmlFor="nee-hbb">Ik wil een wegwerp exemplaar (gaat 1x mee)</label><br/>
-                <input type="radio" value="geen-hbb" name="herbruikbaar"/>
-                <label htmlFor="geen-hbb">Maakt me niet uit, laat me alle opties zien.</label>
-                <h4> Steeltjes?</h4>
-                <input type="radio" value="ja-steel" name="steel"/>
-                <label htmlFor="ja-steel">Ik wil extra hulp bij het eruit halen. Dus een disk met een
+                <label><input type="radio" value="ja-hbb" name="herbruikbaar" onChange={handleOptionChange}
+                              checked={reusable === "ja-hbb"}/>
+                    Ik wil een herbruikbare disk (gaat tot 10 jaar mee)</label><br/>
+                <label><input type="radio" value="nee-hbb" name="herbruikbaar" onChange={handleOptionChange}
+                              checked={reusable === "nee-hbb"}/>
+                    Ik wil een wegwerp exemplaar (gaat 1x mee)</label><br/>
+                <label><input type="radio" value="geen-hbb" name="herbruikbaar" onChange={handleOptionChange}
+                              checked={reusable === "geen-hbb"}/>
+                    Maakt me niet uit, laat me alle opties zien.</label>
+
+                <h4>Steeltjes?</h4>
+                <label> <input type="radio" value="ja-steel" name="steel" onChange={handleOptionChange}
+                               checked={stem === "ja-steel"}/>
+                    Ik wil extra hulp bij het eruit halen. Dus een disk met een
                     steeltje.</label><br/>
-                <input type="radio" value="nee-steel" name="steel"/>
-                <label htmlFor="nee-steel">Het maakt me niet uit, laat me alle opties zien.</label>
-                <button type="submit">laat mijn discs zien!</button>
+                <label><input type="radio" value="nee-steel" name="steel" onChange={handleOptionChange}
+                              checked={stem === "nee-steel"}/>
+                    Het maakt me niet uit, laat me alle opties zien.</label><br/>
+                <button type="button" onClick={resetFilters}>X Reset</button>
             </form>
+            <br/>
 
             <div className="discsTabel">
-                <FilterComponent/>
                 <DataTable
                     title="Menstruatiedisks vergelijkingstabel"
                     columns={columns}
