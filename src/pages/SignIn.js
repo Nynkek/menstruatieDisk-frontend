21596d2888b774f41b3b8d6cc05c902ccc8867fc
Nynke <35605149+Nynkek@users.noreply.github.com>
2022-06-22 19:39:53 +0200

    signin functionaliteiten

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,14 +1,19 @@
-import React, {useContext, useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import React, {useContext, useEffect, useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
 import {AuthContext} from "../context/AuthContext";
 import pageImg
     from "../../../menstruatiedisk-frontend/src/assets/Cupkiezer-Bamboozy-menstruatiedisk-en-cup-vergelijken-in-twee-maten-4356.jpg";
 import YellowContentBox from "../components/pageItems/pageDesignElements/yellowContentBox/YellowContentBox";
 import {useForm} from 'react-hook-form';
+import axios from "axios";
 
 
 function SignIn({headerImageHandler, pageTitleHandler}) {
     const {login, logout, auth} = useContext(AuthContext);
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [addSucces, toggleAddSuccess] = useState(false);
+    const navigate = useNavigate();
     const {register, formState: {errors}, handleSubmit} = useForm({mode: 'onBlur'});
 
     useEffect(() => {
@@ -16,12 +21,26 @@ function SignIn({headerImageHandler, pageTitleHandler}) {
         pageTitleHandler("Inloggen");
     }, []);
 
+    async function signIn(e) {
+        setName(e.username);
+        setPassword(e.password);
+        console.log(e.username, e.password);
+        try {
+            const response = await axios.post(`http://localhost:8080/users/${name}`, {
+                username: name,
+                password: password,
+            },);
+            console.log(response);
+            toggleAddSuccess(true);
+        } catch (error) {
+            console.error('There was an error!', error);
+        };
+    }
+
     return (
         <>
-
         <YellowContentBox>
-
-            <form onSubmit={handleSubmit(login)}>
+            <form onSubmit={handleSubmit(signIn)}>
                 <fieldset>
                     <legend>Gegevens</legend>
                     <label htmlFor="details-username">
@@ -45,6 +64,7 @@ function SignIn({headerImageHandler, pageTitleHandler}) {
                             type="text"
                             id="details-password"
                             {...register("password")}
+                            placeholder="wachtwoord"
                         />
                     </label>
                     {errors.password && <p>{errors.password.message}</p>}<br/>
@@ -63,8 +83,9 @@ function SignIn({headerImageHandler, pageTitleHandler}) {
                             Uitloggen
                         </button>
                     }
+                </fieldset>
             </form>
-        </fieldset>
+
         </YellowContentBox>
     <section className="page-content">
         <p>Heb je nog geen account? <Link to="/registreren" exact>Registreer</Link> je dan eerst.</p>
